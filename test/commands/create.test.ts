bd037ae9bd806b866c688610ca61d5c4f91e0ab1
Tung Du <dinhtungdu@gmail.com>
2019-09-03 20:04:48 +0700

    Update test. Test help only for now. Need to add more later.

--- test/commands/create.test.ts
+++ test/commands/create.test.ts
@@ -3,8 +3,7 @@ import {expect, test} from '@oclif/test'
 describe('create', () => {
   test
     .stdout()
-    .command(['create', 'help'])
-    .exit(0)
+    .command(['help', 'create'])
     .it('runs create help', ctx => {
       expect(ctx.stdout).to.contain('a new Chassis project')
     })
