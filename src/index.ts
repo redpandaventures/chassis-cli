61737b73d44b0828c04fcc6808025378e24faad7
Tung Du <dinhtungdu@gmail.com>
2019-09-10 11:20:17 +0700

    Prepare for using both single and multi command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,51 @@
-export {run} from '@oclif/command'
+import * as Config from '@oclif/config'
+import Help from '@oclif/plugin-help'
+
+import Base from './lib/base'
+
+class Chassis extends Base {
+  static run(argv = process.argv.slice(2), options?: Config.LoadOptions) {
+    return super.run(argv, options || module.parent && module.parent.parent && module.parent.parent.filename || __dirname)
+  }
+
+  async init() {
+    let [id, ...argv] = this.argv
+    await this.config.runHook('init', {id, argv})
+    return super.init()
+  }
+
+  async run() {
+    if (this.argv.length > 0) {
+      let [id, ...argv] = this.argv
+      this.parse({strict: false, '--': false, ...this.ctor as any})
+      if (!this.config.findCommand(id)) {
+        let topic = this.config.findTopic(id)
+        if (topic) return this._help()
+      }
+      return this.config.runCommand(id, argv)
+    }
+
+    this.log('Hello from Chassis')
+  }
+
+  protected _helpOverride(): boolean {
+    if (['-v', '--version', 'version'].includes(this.argv[0])) return this._version() as any
+    if (['-h', 'help'].includes(this.argv[0])) return true
+    for (let arg of this.argv) {
+      if (arg === '--help') return true
+      if (arg === '--') return false
+    }
+    return false
+  }
+
+  protected _help() {
+    const HHelp: typeof Help = require('@oclif/plugin-help').default
+    const help = new HHelp(this.config)
+    help.showHelp(this.argv)
+    return this.exit(0)
+  }
+}
+
+export function run(argv = process.argv.slice(2), options?: Config.LoadOptions) {
+  return Chassis.run(argv, options)
+}
--- src/lib/base.ts
+++ src/lib/base.ts
@@ -7,5 +7,7 @@ export default abstract class Base extends Command {
       this.error(`Vagrant not found!
 You need Vagrant and Virtual Box to use Chassis.
 Get them here: http://docs.chassis.io/en/latest/quickstart/#prerequisites`)
+
+    return super.init()
   }
 }
