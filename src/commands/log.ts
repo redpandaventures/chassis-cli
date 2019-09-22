8bb101867a35eac3be753b5eeedf9b9a0dc537b3
Tung Du <dinhtungdu@gmail.com>
2019-09-22 12:00:50 +0700

    make command aware of context

--- src/commands/log.ts
+++ src/commands/log.ts
@@ -4,6 +4,7 @@ import open from 'open'
 import {homedir} from 'os'
 
 import Base from '../lib/base'
+import {getLocalConfig, isChassisDir} from '../lib/helpers'
 
 export default class Log extends Base {
   static description = 'View chassis logs'
@@ -15,7 +16,20 @@ export default class Log extends Base {
   async run() {
     const logDir = `${homedir()}/.chassis/logs`
     const logFiles = (await fs.readdir(logDir))
-      .filter(f => f.substr(-4) === '.log')
+      .filter(f => {
+        if (! isChassisDir)
+          return f.substr(-4) === '.log'
+
+        const domain = getLocalConfig('hosts')
+
+        if (domain.length === 0)
+          return f.includes(domain[0])
+
+        return false
+      })
+
+    if (logFiles.length === 0)
+      this.log("Can't find any log for current project")
 
     let responses: any = await inquirer.prompt([
       {
